185cc3b2c051b717f74084a263b9621a0e8f0bcb
tb <tobbecnet@gmx.de>
2018-05-25 14:51:09 +0200

    solrcloud running with replica: 1.

---
+++ gcloud/kube-init-solrcloud/shutdown-setup-solrcloud.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+kubectl delete configmap solr-config 
+
+kubectl delete pvc solrc-data-solrc-ss-0
+kubectl delete pvc solrc-data-solrc-ss-1
+kubectl delete pvc solrc-data-solrc-ss-2
+
+kubectl delete pv solrc-data-0
+kubectl delete pv solrc-data-1
+kubectl delete pv solrc-data-2
+
+kubectl delete statefulset solrc-ss
+kubectl delete service solr-service
+
+echo " -- SolR PV/StatefulSet/Service created -- "
